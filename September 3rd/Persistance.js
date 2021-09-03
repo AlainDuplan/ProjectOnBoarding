9ec80209d1e788f8b0dd2b28873c677e2bf6014f
Alain-Duplan <aduplan@umass.edu>
2021-09-03 12:04:18 -0400

    logic

---
+++ September 3rd/Persistance.js
@@ -0,0 +1,9 @@
+console.log(document.getElementsByTagName("button").);
+
+function additivePersistence(x) {
+    let y = 1;
+    console.log(x);
+}
+
+
+document.getElementsByTagName("button").onclick = additivePersistence(document.getElementById("quantity"));
